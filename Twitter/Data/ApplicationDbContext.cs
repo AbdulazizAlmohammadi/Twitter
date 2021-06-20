2e535e99ea22f6ab7602e0e316894dbe2a27b5f5
teefeid <taef_415@hotmail.com>
2021-06-20 13:23:08 +0300

    Add db and Relations Okay

---
+++ Twitter/Data/ApplicationDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Twitter.Models;
+
+namespace Twitter.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        //add table Tweet 
+        public DbSet<TweetModel> Tweets { get; set; }
+        //add table Follow 
+        public DbSet<FollowModel> Follow { get; set; }
+        //add table User 
+        public DbSet<UserModel> Users { get; set; }
+        //add table Profile 
+        public DbSet<ProfileModel> Profiles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<UserModel>()
+                .HasMany(u => u.Followers)
+                .WithMany(u => u.Following)
+                .UsingEntity<FollowModel>(
+                    j => j
+                        .HasOne(e=>e.user)
+                        .WithMany(ee=>ee.UserFollowers)
+                        .HasForeignKey(t=>t.userId),
+                    j => j
+                        .HasOne(e=>e.follower)
+                        .WithMany(e=>e.UserFollowing)
+                        .HasForeignKey(t=>t.followerId),
+                    j =>
+                    {
+                        j.HasKey(t => new { t.userId, t.followerId });
+                    }
+                );
+        }
+    }
+}
+
---
+++ Twitter/Migrations/20210620100056_AddAllTables.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Twitter.Migrations
+{
+    public partial class AddAllTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    userId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    userEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.userId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Follow",
+                columns: table => new
+                {
+                    userId = table.Column<int>(type: "int", nullable: false),
+                    followerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Follow", x => new { x.userId, x.followerId });
+                    table.ForeignKey(
+                        name: "FK_Follow_Users_followerId",
+                        column: x => x.followerId,
+                        principalTable: "Users",
+                        principalColumn: "userId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Follow_Users_userId",
+                        column: x => x.userId,
+                        principalTable: "Users",
+                        principalColumn: "userId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Profiles",
+                columns: table => new
+                {
+                    ProfileId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProfilePicture = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Bio = table.Column<string>(type: "nvarchar(160)", maxLength: 160, nullable: true),
+                    TotalFollowers = table.Column<int>(type: "int", nullable: false),
+                    TotalFollowing = table.Column<int>(type: "int", nullable: false),
+                    NumberOfTweets = table.Column<int>(type: "int", nullable: false),
+                    DateOfJoin = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profiles", x => x.ProfileId);
+                    table.ForeignKey(
+                        name: "FK_Profiles_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "userId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tweets",
+                columns: table => new
+                {
+                    TweetId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    TweetContent = table.Column<string>(type: "nvarchar(280)", maxLength: 280, nullable: true),
+                    TweetDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ProfilePicture = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tweets", x => x.TweetId);
+                    table.ForeignKey(
+                        name: "FK_Tweets_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "userId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Follow_followerId",
+                table: "Follow",
+                column: "followerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profiles_UserId",
+                table: "Profiles",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tweets_UserId",
+                table: "Tweets",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Follow");
+
+            migrationBuilder.DropTable(
+                name: "Profiles");
+
+            migrationBuilder.DropTable(
+                name: "Tweets");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
